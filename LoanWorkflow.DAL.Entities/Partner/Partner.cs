a80deb5f5ab119edf52f20de022a20157a8dcaf2
Anna12146 <annakoshetsyan.11@mail.ru>
2024-03-21 16:35:00 +0400

    add partner class

--- LoanWorkflow.DAL.Entities/Partner/Partner.cs
+++ LoanWorkflow.DAL.Entities/Partner/Partner.cs
@@ -1,12 +1,12 @@
 ﻿using LoanWorkflow.DAL.Entities.Abstractions;
-using LoanWorkflow.DAL.Entities.Approvers;
-using Microsoft.AspNetCore.Identity;
+using LoanWorkflow.DAL.Entities.Users;
 
-namespace LoanWorkflow.DAL.Entities.Users
+namespace LoanWorkflow.DAL.Entities
 {
     public class Partner : Entity
     {
         public long Id { get; set; }
         public string Name { get; set; }
+        public ICollection<User> Users { get; set; }
     }
 }
