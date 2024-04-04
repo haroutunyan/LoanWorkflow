49d05e4c271132333a3cb95e8558ee7b833c4b00
Spartak Harutyunyan <haroutunyan@gmail.com>
2024-04-04 18:31:24 +0400

    fixed path

--- LoanWorkflow.Services/Ekeng/EkengService.cs
+++ LoanWorkflow.Services/Ekeng/EkengService.cs
@@ -4,40 +4,42 @@ using LoanWorkflow.Services.DTO.Ekeng.Ces;
 using LoanWorkflow.Services.DTO.Ekeng.ECivil;
 using LoanWorkflow.Services.DTO.Ekeng.Police;
 using LoanWorkflow.Services.Interfaces.Ekeng;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace LoanWorkflow.Services.Ekeng
 {
     public class EkengService(
-        HttpClient httpClient) : IEkengService
+        HttpClient httpClient,
+        IConfiguration config) : IEkengService
     {
         public async Task<AvvResponse> GetAvvData(string ssn)
         {
-            var data = File.ReadAllText("C:\\Users\\User\\Downloads\\Telegram Desktop\\avv.txt");
+            var data = File.ReadAllText(Path.Combine(config["PersonalInfo"], "avv.txt"));
             return await Task.FromResult(JsonConvert.DeserializeObject<AvvResponse>(data));
         }
 
         public async Task<PhysicalPersonBusinessResult> GetBusinessRegisterData(string ssn)
         {
-            var data = File.ReadAllText("C:\\Users\\User\\Downloads\\Telegram Desktop\\businessregister.txt");
+            var data = File.ReadAllText(Path.Combine(config["PersonalInfo"], "businessregister.txt"));
             return await Task.FromResult(JsonConvert.DeserializeObject<PhysicalPersonBusinessResult>(data));
         }
 
         public async Task<CesResult> GetCesData(string ssn)
         {
-            var data = File.ReadAllText("C:\\Users\\User\\Downloads\\Telegram Desktop\\ces.txt");
+            var data = File.ReadAllText(Path.Combine(config["PersonalInfo"], "ces.txt"));
             return await Task.FromResult(JsonConvert.DeserializeObject<CesResult>(data));
         }
 
         public async Task<CivilResult> GetCivilResult(string ssn)
         {
-            var data = File.ReadAllText("C:\\Users\\User\\Downloads\\Telegram Desktop\\ecivil.txt");
+            var data = File.ReadAllText(Path.Combine(config["PersonalInfo"], "ecivil.txt"));
             return await Task.FromResult(JsonConvert.DeserializeObject<CivilResult>(data));
         }
 
         public async Task<VehiclesResult> GetVehicleData(string ssn)
         {
-            var data = File.ReadAllText("C:\\Users\\User\\Downloads\\Telegram Desktop\\vehicleinfo.txt");
+            var data = File.ReadAllText(Path.Combine(config["PersonalInfo"], "vehicleinfo.txt"));
             return await Task.FromResult(JsonConvert.DeserializeObject<VehiclesResult>(data));
         }
     }
